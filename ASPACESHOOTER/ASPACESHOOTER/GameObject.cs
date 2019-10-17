aae534c98889c2bdd794d26ec25f0a2bb4f2b161
Hennnrik <43605104+Hennnrik@users.noreply.github.com>
2019-10-17 13:25:22 +0200

    gameobject stuff

--- ASPACESHOOTER/ASPACESHOOTER/GameObject.cs
+++ ASPACESHOOTER/ASPACESHOOTER/GameObject.cs
@@ -12,22 +12,54 @@ namespace ASPACESHOOTER
     class GameObject
     {
 
-        Texture2D gfx;
-        Vector2 posision;
+        protected Texture2D texture; //Texture
+        protected Vector2 posision; //Spawn point
 
-        public GameObject(Texture2D gfx, float X, float Y)
+        //==========================================================================
+        //===Konstructor for objects
+        public GameObject(Texture2D texture, float X, float Y)
         {
 
-            this.gfx = gfx;
+            this.texture = texture;
             this.posision.X = X;
-
-
+            this.posision.Y = Y;
+            
         }
 
+        //=========================================================================
+        //===Draws object using texture & spawn point
         public void Draw(SpriteBatch spriteBach)
         {
-            
+
+            spriteBach.Draw(texture, posision, Color.White);
 
         }
+
+        //==========================================================================
+        //===Properties of object
+        public float X { get { return posision.X; } }
+        public float Y { get { return posision.Y; } }
+        public float Width { get { return texture.Width; } }
+        public float Height { get { return texture.Height; } }
+    }
+
+    //====================================================================================================================================================
+    //===Moving object
+    class MovingObject : GameObject
+    {
+
+        protected Vector2 speed; //Speed
+
+        //==========================================================================
+        //===Konstructor for moving object
+        public MovingObject(Texture2D texture, float X, float Y, float speedX, float speedY) : base(texture, X, Y)
+        {
+
+            this.speed.X = speedX;
+            this.speed.Y = speedY;
+
+        }
+
+
     }
 }
